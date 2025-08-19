adfa95fad8cabbfcda121861f506001a68e67c91
Nehme Haber <nehmehabr19@gmail.com>
2025-08-19 15:20:26 +0300

    minor change in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "../includes/irc.hpp"
 
 
-int main()
+int main(int argc, char **argv)
 {
+    (void)argv;
+    if (argc != 3)
+    {
+        std::cout<<"Invalid usage."<<std::endl;
+        return (1);
+    }
     std::cout<<"Welcome to IRC"<<std::endl;
     return (0);
 }
